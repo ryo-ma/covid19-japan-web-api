b2899d242116b4b515b2d652b560a3596fac9fc4
ryo-ma <saka_ro@yahoo.co.jp>
2020-03-29 21:56:13 +0900

    Add base controller

--- index.py
+++ index.py
@@ -1,9 +1,25 @@
-from sanic import Sanic
+from sanic import Blueprint, Sanic
 from sanic.response import json
 
 app = Sanic()
+
+apiv1 = Blueprint('apiv1', url_prefix='/api/v1')
 
 
 @app.route('/')
 async def index(request):
     return json({'result': 'ok'})
+
+
+@apiv1.route('/all')
+async def all(request):
+    return json({'result': 'all'})
+
+
+@apiv1.route('/prefectures')
+async def prefecture(request):
+    return json({'result': 'prefecture'})
+
+
+api = Blueprint.group(apiv1)
+app.blueprint(api)
