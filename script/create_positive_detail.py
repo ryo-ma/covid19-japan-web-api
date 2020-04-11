be00139327a6a010c3dee71584c2b4c15348a838
Kiyohito Itoh <kiyohito.itoh@gmail.com>
2020-04-11 12:48:23 +0900

    Fix flake8 errors

--- script/create_positive_detail.py
+++ script/create_positive_detail.py
@@ -5,12 +5,12 @@ import textwrap
 POSITIVE_DETAIL_DATA_PATH = 'data/2019-ncov-japan/Data/positiveDetail.csv'
 OUTPUT_JSON_PATH = 'data/created_json/positive_detail.json'
 OUTPUT_JSON_PATH_FORMAT = 'data/created_json/positive_detail_by_prefecture/{0}.json'
-
 PREFECTURES = textwrap.dedent('''\
 北海道,青森県,岩手県,宮城県,秋田県,山形県,福島県,茨城県,栃木県,群馬県,埼玉県,千葉県,東京都,神奈川県,新潟県,富山県,\
 石川県,福井県,山梨県,長野県,岐阜県,静岡県,愛知県,三重県,滋賀県,京都府,大阪府,兵庫県,奈良県,和歌山県,鳥取県,島根県,\
 岡山県,広島県,山口県,徳島県,香川県,愛媛県,高知県,福岡県,佐賀県,長崎県,熊本県,大分県,宮崎県,鹿児島県,沖縄県\
 ''').split(',')
+
 
 def create_json_file():
     header = ('code', 'announcement_date', 'src', 'prefecture', 'residence_prefecture',
