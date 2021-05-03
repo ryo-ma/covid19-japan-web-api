6ad03d2a5898c9972dd1061fffabb26d019a493d
ryo-ma <saka_ro@yahoo.co.jp>
2021-05-03 14:14:47 +0900

    Fix data_manager

--- src/data_manager.py
+++ src/data_manager.py
@@ -5,13 +5,18 @@ from .const import (PREFECTURES_JSON_PATH, PREDICTION_TOTAL_JSON_PATH,
 
 class DataManager:
     def __init__(self):
-        with open(PREFECTURES_JSON_PATH) as f:
-            self.prefectures_json = f.read()
-        with open(HISTORY_TOTAL_JSON_PATH) as f:
-            self.history_total_json = f.read()
-        with open(TODAY_TOTAL_JSON_PATH) as f:
-            self.today_total_json = f.read()
-        with open(PREDICTION_TOTAL_JSON_PATH) as f:
-            self.prediction_total_json = f.read()
-        with open(STATISTICS_JSON_PATH) as f:
-            self.statistics_json = f.read()
+        self.prefectures_json = '[]'
+        self.history_total_json = '[]'
+        self.today_total_json = '[]'
+        self.prediction_total_json = '[]'
+        self.statistics_json = '[]'
+        # with open(PREFECTURES_JSON_PATH) as f:
+        #     self.prefectures_json = f.read()
+        # with open(HISTORY_TOTAL_JSON_PATH) as f:
+        #     self.history_total_json = f.read()
+        # with open(TODAY_TOTAL_JSON_PATH) as f:
+        #     self.today_total_json = f.read()
+        # with open(PREDICTION_TOTAL_JSON_PATH) as f:
+        #     self.prediction_total_json = f.read()
+        # with open(STATISTICS_JSON_PATH) as f:
+        #     self.statistics_json = f.read()
